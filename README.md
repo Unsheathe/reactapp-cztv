afe731b3ce3a084270d96a0a5101cf8b5254a5c6
M-Ub_Hub <michael-gibbub@disengage.co>
2024-07-02 13:55:26 -0600

    the remote finally displays! positioning needed next. Also a fetch added for the czech tv, which is being denied bc of a CORS policy. Will postpone that issue for later

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 media from
 
+
+backdrop
 https://images.unsplash.com/photo-1593784991251-92ded75ea290?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
 
+
+remote control
 https://imgs.search.brave.com/swgSwBhGEFTXcXhr0VS4InruSLlyRYxOaGVpbT1W0L8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jYW1l/cmFyZXZpZXdzLmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAy/NC8wMi9jYW5vbi1i/cmUxLXJlbW90ZS1j/b250cm9sLnBuZw
--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-//import logo from './logo.svg';
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const [offsetX, setoffsetX] = useState(0)
   const [offsetY, setoffsetY] = useState(0)
 
+  const now = new Date() 
+  const year = now.getFullYear()
+  const month = now.getMonth()
+  const day = now.getDay()
+
+  const [channel, setchannel] = useState(1) //valid channels: [1, 2, 24, 4, 5, 6, 7]
+  const [tvon, settvon] = useState(0)
+
   useEffect(() => {
+
+    const changeChannel = () => {
+      fetch(`https://www.ceskatelevize.cz/services-old/programme/xml/schedule.php?user=test&date=${day}.${month}.${year-1}&channel=ct${channel}&json=1`)
+        .then(ret => ret.json())
+        .then(data => {
+          console.log(data)
+          return data
+        })
+    }
+    
+    const toggleTV = () => {
+      if (tvon) {settvon = 0}
+      else {
+        settvon = 1
+        changeChannel()
+      }
+    }
+
     const shift = (event) => {
       const { innerWidth, innerHeight } = window;
             const { clientX, clientY } = event;
@@ -17,18 +42,24 @@ function App() {
     }
     
     document.addEventListener('mousemove', shift) 
-
-    //put in returns statement?
+    document.addEventListener('mousedown', changeChannel)
     return(()=>{
       document.removeEventListener('mousemove', shift)
+      document.removeEventListener('mousedown', changeChannel)
     })
-    
   }, [])
 
-  return (
-    <div className='background' style={{transform:`translate(${offsetX}%, ${offsetY}%)`}}></div>
-  )
-  
+  return (<>
+    <div className='background' style={{transform:`translate(${-offsetX}%, ${-offsetY}%)`}}>
+      
+    </div>
+    <div className='remote'>
+      <img src={require('./media/control.png')} alt='remote'/>
+    </div>
+    <div className='remoteButtons'>
+      <button type='button'>test button</button>
+    </div>
+  </>)
 }
 
 export default App;
